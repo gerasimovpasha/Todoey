357fffaabb327e72f2759624c77ef22d4d6b6e2c
Pavel Gerasimov <pavelgerasimov@macbook-pro.local>
2018-03-08 16:34:11 +0300

    Conver to MVC Pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Pavel Gerasimov on 07.03.2018.
+//  Copyright © 2018 Pavel Gerasimov. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+    
+    var title = ""
+    var done = false
+    
+}
